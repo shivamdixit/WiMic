fc9cf8797e65096763c16ff80b80e3637f598712
Shivam Dixit <shivamd001@gmail.com>
2015-04-03 23:55:57 +0530

    Added dummy cards list
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/src/main/java/in/ac/lnmiit/wimic/MainActivity.java
+++ app/src/main/java/in/ac/lnmiit/wimic/MainActivity.java
@@ -2,17 +2,34 @@ package in.ac.lnmiit.wimic;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private List<String> rooms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView;
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        this.createDummyData();
+        RVAdapter adapter = new RVAdapter(rooms);
+        recyclerView.setAdapter(adapter);
     }
 
 
@@ -37,5 +54,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createDummyData() {
+        rooms = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            rooms.add("Room " + i);
+        }
     }
 }
--- app/src/main/java/in/ac/lnmiit/wimic/RVAdapter.java
+++ app/src/main/java/in/ac/lnmiit/wimic/RVAdapter.java
@@ -1,7 +1,55 @@
 package in.ac.lnmiit.wimic;
 
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
- * Created by shivam on 4/3/15.
+ * @author Shivam Dixit <shivamd001 at gmail.com>
  */
-public class RVAdapter {
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.RoomViewHolder> {
+
+    List<String> rooms;
+
+    RVAdapter(List<String> rooms) {
+        this.rooms = rooms;
+    }
+
+    public static class RoomViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView name;
+
+        RoomViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView) itemView.findViewById(R.id.card_view);
+            name = (TextView) itemView.findViewById(R.id.info_text);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return rooms.size();
+    }
+
+    @Override
+    public RoomViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.cards_layout, viewGroup, false);
+        return new RoomViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RoomViewHolder roomViewHolder, int i) {
+        roomViewHolder.name.setText(rooms.get(i));
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
 }
