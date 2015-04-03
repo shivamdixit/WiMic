e90a5a1e43bfe749a8900b55669a1d24082fc265
Shivam Dixit <shivamd001@gmail.com>
2015-04-03 21:11:38 +0530

    Added ActionBar Item - Refresh
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/src/main/java/in/ac/lnmiit/wimic/MainActivity.java
+++ app/src/main/java/in/ac/lnmiit/wimic/MainActivity.java
@@ -3,6 +3,7 @@ package in.ac.lnmiit.wimic;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
@@ -18,8 +19,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
