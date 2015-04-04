47c6886c5a2f1d13e9b4d2f8b3186e44de28b4c2
Shivam Dixit <shivamd001@gmail.com>
2015-04-04 11:13:06 +0530

    Removed spurious import
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/src/main/java/in/ac/lnmiit/wimic/MainActivity.java
+++ app/src/main/java/in/ac/lnmiit/wimic/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View.OnClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
