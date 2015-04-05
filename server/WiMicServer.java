922c341c433b81194f5d0560b9ab30b240da8fe4
Shivam Dixit <shivamd001@gmail.com>
2015-04-05 10:25:47 +0530

    Added name option in server and modified message format
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- server/WiMicServer.java
+++ server/WiMicServer.java
@@ -1,15 +1,21 @@
 import java.net.InetAddress;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
+import java.util.Scanner;
 
 public class WiMicServer implements Runnable {
     private DatagramSocket socket;
 
     private final int PORT = 9876;
-
     private final String LOCALHOST = "0.0.0.0";
     private final String DISC_MESSAGE = "WIMIC_DISCOVER_REQ";
     private final String ACK_MESSAGE = "WIMIC_DISCOVER_ACK";
+
+    private String name = "WiMic Server";
+
+    WiMicServer(String name) {
+        this.name = name;
+    }
 
     public void run() {
         try {
@@ -17,7 +23,7 @@ public class WiMicServer implements Runnable {
             socket.setBroadcast(true);
 
             while (true) {
-                System.out.println("WiMic Server is ready");
+                System.out.println(name + " is ready");
 
                 byte[] receiveBuffer = new byte[15000];
                 DatagramPacket packet = new DatagramPacket(
@@ -31,7 +37,7 @@ public class WiMicServer implements Runnable {
                 if (message.equals(DISC_MESSAGE)) {
                     System.out.println("Packet received from " + packet.getAddress());
 
-                    byte[] sendData = ACK_MESSAGE.getBytes();
+                    byte[] sendData = (ACK_MESSAGE + ";" + this.name).getBytes();
                     DatagramPacket sendPacket = new DatagramPacket(
                         sendData,
                         sendData.length,
@@ -49,16 +55,15 @@ public class WiMicServer implements Runnable {
         }
     }
 
-    public static WiMicServer getInstance() {
-        return WiMicServerHolder.INSTANCE;
-    }
-
-    private static class WiMicServerHolder {
-        private static final WiMicServer INSTANCE = new WiMicServer();
-    }
-
     public static void main(String[] args) {
-        Thread serverThread = new Thread(WiMicServer.getInstance());
+        Scanner sc;
+        sc = new Scanner(System.in);
+        String name;
+
+        System.out.println("Enter room name:");
+        name = sc.next();
+
+        Thread serverThread = new Thread(new WiMicServer(name));
         serverThread.start();
     }
 }
