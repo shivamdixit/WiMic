034cf45393e3302a9126a412268d5d12b3c2f5f0
Shivam Dixit <shivamd001@gmail.com>
2015-04-05 19:36:21 +0530

    Added PIN authentication in server
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- server/WiMicServer.java
+++ server/WiMicServer.java
@@ -10,12 +10,17 @@ public class WiMicServer implements Runnable {
     private final String LOCALHOST = "0.0.0.0";
     private final String DISC_MESSAGE = "WIMIC_DISCOVER_REQ";
     private final String ACK_MESSAGE = "WIMIC_DISCOVER_ACK";
+    private final String JOIN_MESSAGE = "WIMIC_JOIN_PASSWORD";
+    private final String JOIN_SUCCESS = "WIMIC_JOIN_SUCCESS";
+    private final String JOIN_FAIL = "WIMIC_JOIN_FAILURE";
 
     private String name = "WiMic Server";
+    private int pin;
 
-    WiMicServer(String name) {
+    WiMicServer(String name, int pin) {
         // Ensure first alphabet is capital
         this.name = name.substring(0, 1).toUpperCase() + name.substring(1);
+        this.pin = pin;
     }
 
     public void run() {
@@ -23,8 +28,9 @@ public class WiMicServer implements Runnable {
             socket = new DatagramSocket(PORT, InetAddress.getByName(LOCALHOST));
             socket.setBroadcast(true);
 
+            System.out.println(name + " is ready. Your pin is: " + pin);
+
             while (true) {
-                System.out.println(name + " is ready");
 
                 byte[] receiveBuffer = new byte[15000];
                 DatagramPacket packet = new DatagramPacket(
@@ -47,13 +53,50 @@ public class WiMicServer implements Runnable {
                     );
 
                     socket.send(sendPacket);
-
                     System.out.println("Sent response");
+
+                } else if (message.contains(JOIN_MESSAGE)) {
+                    String[] request = message.split(";");
+                    byte[] sendData;
+
+                    System.out.println(request.length);
+
+                    if (request.length >= 2 && validatePin(request[1])) {
+                        sendData = JOIN_SUCCESS.getBytes();
+                        System.out.println("PIN success!");
+                    } else {
+                        sendData = JOIN_FAIL.getBytes();
+                        System.out.println("Invalid PIN");
+                    }
+
+                    DatagramPacket sendPacket = new DatagramPacket(
+                        sendData,
+                        sendData.length,
+                        packet.getAddress(),
+                        packet.getPort()
+                    );
+
+                    socket.send(sendPacket);
                 }
             }
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    private boolean validatePin(String pin) {
+        String regex = "\\d+";
+        int pinInt;
+
+        if (pin.matches(regex)) {
+            pinInt = Integer.parseInt(pin);
+
+            if (this.pin == pinInt) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
@@ -64,7 +107,10 @@ public class WiMicServer implements Runnable {
         System.out.println("Enter room name:");
         name = sc.next();
 
-        Thread serverThread = new Thread(new WiMicServer(name));
+        // Generate a random pin
+        double rand = Math.random() * 8999 + 1000;
+
+        Thread serverThread = new Thread(new WiMicServer(name, (int) rand));
         serverThread.start();
     }
 }
